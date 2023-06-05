595cced8ba4430cbd7eb05c7c651fff051d49fd2
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-06-05 17:07:13 +0100

    initial state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div></div>;
 };
+
+/* 
+-header:
+  navlink: home page, movies page
+-main:
+    outlet:
+      *Home page* (page) path=http:API
+      -tranding title
+      -tranding list (movies) => click on item => request=  path=http:API/movies/id 
+                                          movie details (page) : 
+                                          - main info about movie
+                                          - outlet:
+                                              - paragraph
+                                              - cast (component) path=http:API/movies/id/cast
+                                              - reviews (component) path=http:API/movies/id/reviews
+       *Movies page* (page) path=http:API/movies                                   
+      - search
+      - response list path=http:API/movies?query={searchQuery}
+
+*/
